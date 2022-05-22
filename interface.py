99309a13c697bc74ad12497aa1b6e1a6b1ae27ce
Carlos Martínez Suárez <87413904+camacar36@users.noreply.github.com>
2022-05-22 13:46:14 +0200

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,8 +1,7 @@
+from pickle import FALSE
 from features import *
 from tramas import *
 from lib import *
-import serial
-arduino = serial.Serial('/dev/ttyACM0', 9600)
 
 from threading import Thread
 
@@ -22,12 +21,17 @@ pygame.display.set_icon(pygame.image.load(WINDOW_ICON))
 # ---------------------------------------
 DoIt =  True
 
-# -----------------------
-# Initializing the APIS |
-# -----------------------
+write = True # para escritura de trama
+settings = False
+current_switch = "3"
+
+
+# ----------------------
+# Initializing the API |
+# ----------------------
 cw = CurrentWeather()   # This is the call for Current weather API
 fw = ForecastWeather()  # This is the call for Forecast weather API
-re = REDataAPI()
+re = REDataAPI()        # This is the call for the REData API
 
 # -------------------------------------
 # Initializing the items of the house |
@@ -39,15 +43,17 @@ items = Items()
 # Initializing the tcp/ip of the house |
 # --------------------------------------
 # Creates the tcp/ip connection object
-trama   = "0"
+trama   = str(int(0))
 data    = ""
+pressed = bool(False)
+
 
 class TCPIPconnection():
     def __init__(self):
         # TCP/IP socket declaration
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect(IP,PORT)
-        self.check_connection()
+        # self.check_connection()
         
     def connect(self, ip, port):
         # Connecting to server
@@ -69,10 +75,10 @@ class TCPIPconnection():
         
         self.updateItems()
         
-        # print("--------")
-        # print("trama: ", type(str(trama)), trama)
-        # print("data: ", type(data), type(data[0]), data)
-        # print("items: ", items.led, items.tv, items.air[1],items.appliance, items.blind[1])
+        print("--------")
+        print("trama: ", type(str(trama)), trama)
+        print("data: ", type(data), type(data[0]), data)
+        print("items: ", items.led, items.tv, items.air,items.appliance, items.blind)
         
         
     def to_list(self,string):
@@ -82,18 +88,16 @@ class TCPIPconnection():
     
     def updateItems(self):
         global items, data
-        items.led = int(data[0])
-        items.tv = int(data[1])
-        items.air[1] = int(data[2])
-        items.appliance = int(data[3])
-        items.blind[1] = int(data[4])
+        items.led = int(data[1])
+        items.tv = int(data[2])
+        items.air[0] = items.air_onoff = int(data[3])
+        items.appliance = int(data[4])
+        items.blind = int(data[4])
 
     def close(self):
         self.sock.close()
 
  
-# tcpip = TCPIPconnection()
-# tcpip.connect(IP,PORT)
 # tcpip = TCPIPconnection()
 
 # ---------------------------------
@@ -168,17 +172,86 @@ def moving_to_setting_window():
     global current_screen
     current_screen = 7
 
+def change_air_on():
+    global trama,items
+    items.air[0] = 1
+
+    trama = items.get_trama()
+
+def change_air_off():
+    global trama,items
+    items.air[0] = 0
+
+    trama = items.get_trama()
+
+def change_air_mode_to_man():
+    global items, trama
+    items.air[1] = 1
+
+    trama = items.get_trama()
+
+def change_air_mode_to_auto():
+    global items, trama
+    items.air[1] = 2 
+
+    trama = items.get_trama()
+    
+def up_temperature():
+    global items, trama
+    if items.air[3] < 30 and not items.air[0] == 0:    
+        items.air[3] = items.air[3] + 1
+    
+    trama = items.get_trama()
+
+def down_temperature():
+    global items, trama
+    if items.air[3] > 15 and not items.air[0] == 0:  
+        items.air[3] = items.air[3] - 1
+
+    trama = items.get_trama()
+
+def up_blind():
+    global items, trama
+    if items.blind == 0 or items.blind == 2:    
+        items.blind = 1 
+    else:
+        items.blind = 0
+    trama = items.get_trama()
+
+def down_blind():
+    global items,trama
+    if items.blind == 0 or items.blind == 1:   
+        items.blind = 2
+    else:
+        items.blind = 0
+    trama = items.get_trama()
+
+
 def send_settings_data():
-    pass
+    global trama, items, current_screen, settings
+    trama = items.get_trama_settings()
+    current_screen = 0
+    settings = True
+
+
+
+    
 
 # Let's just create a vector that contains all the functions just so it will be easier to call the function print of the main buttons
-main_functions      = [moving_to_weather_window, moving_to_light_window, moving_to_tv_window, moving_to_air_window, moving_to_appliance_window ,moving_to_blind_window, moving_to_setting_window]
+main_functions      = [moving_to_weather_window, moving_to_light_window, moving_to_tv_window,
+                        moving_to_air_window, moving_to_appliance_window ,
+                        moving_to_blind_window, moving_to_setting_window]
+
 weather_functions   = [moving_to_main_window]
 lights_functions    = [moving_to_main_window]
 tv_functions        = [moving_to_main_window]
-air_functions       = [moving_to_main_window]
+
+air_functions       = [moving_to_main_window, change_air_on, change_air_off, 
+                        change_air_mode_to_man, change_air_mode_to_auto, 
+                        up_temperature, down_temperature]
+
 appliance_functions = [moving_to_main_window]
-blinds_functions    = [moving_to_main_window]
+blinds_functions    = [moving_to_main_window, up_blind, down_blind]
 settings_functions  = [moving_to_main_window, send_settings_data]
 
 # --------------------------
@@ -214,10 +287,59 @@ end2    = start2
 # -------------------------------------
 # Infinite loop that controls the APP |
 # -------------------------------------
+
+def readTrama():
+    global data, current_switch,settings
+    with open('trama2.txt', 'r') as f:
+        aux = f.read()
+
+        print("data: ", aux, " current_switch: ", current_switch)
+
+        #if(not aux.startswith(current_switch) and len(aux) > 1 ):
+        print("entrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
+        data = aux
+        items.led = int(data[1])
+        items.tv = int(data[2])
+        if data[3] == 0:
+            print("hooooooooooooooooooooooooooooooooo")
+            items.air[0] = 0
+        elif data[3] == 1 or data[3] == 2:
+            print("hooooooooooooooooooooooooooooooooo")
+            items.air[0] = 1
+            items.air[2] = data[3]
+        items.appliance = int(data[4])
+        items.blind = int(data[5])
+
+        if current_switch == "3":
+            current_switch = "4"
+        elif current_switch == "4":
+            current_switch = "3"
+
+        settings = False
+        
+
+def writeTrama():
+    global trama, settings, current_switch
+
+    if not settings:
+        trama = items.get_trama()
+    else:
+        trama = items.get_trama_settings()
+        
+
+    print("trama: ", trama)
+
+    with open('trama1.txt', 'w') as f:
+        f.write(current_switch + trama)
+
+
+
 while DoIt:
-    # t = Thread(target=tcpip.communicate(), )
-    # t.setDaemon = True
+    # t = Thread(target=tcpip.communicate())
     # t.start()
+
+   
+
 
     # Handling the events - Stop the app
     for event in pygame.event.get():
@@ -231,47 +353,42 @@ while DoIt:
     if (end-start) >= TIMESTAMP:        
         # This calls the OpenWeather API and get the information that is required
         cw.get_weather_info()
-        fw.get_weather_info()
 
         # Let´s restart the timer so it does another call to the API in the selected timestamp
         start   = time.time()
         end     = start
-
-    if (end2-start2) >= TIMESTAMP:
+    
+    if (end2-start2) >= TIMESTAMP2:
         re.get_data_info()
 
         # Let's restart the timer so it does another call to the API in the selected timestamp
         start2  = time.time()
         end2    = start2
-      
+        
     # Checking which window we have to print, but first we fill the background with a default color
     screen.fill(HIPER_LIGHT_BLUE)
-
-    # print(pygame.mouse.get_pos())
-
-    # t.join()
-
+    
     if current_screen == 0:
         main_window.print_main_window(screen, main_functions, cw, rain)
     elif current_screen == 1:
         weather_window.print_weather_window(screen, weather_functions, cw, fw, rain)
     elif current_screen == 2:
         trama = light_window.print_lights_window(screen, lights_functions, items)
-        # print(trama)
     elif current_screen == 3:
-        tv_window.print_tv_window(screen, tv_functions, items)
+        trama = tv_window.print_tv_window(screen, tv_functions, items)
     elif current_screen == 4:
         air_window.print_air_window(screen, air_functions, items)
     elif current_screen == 5:
-        appliance_window.print_appliance_window(screen, appliance_functions, items)
+        trama = appliance_window.print_appliance_window(screen, appliance_functions, items)
     elif current_screen == 6:
-        blind_window.print_blind_window(screen, blinds_functions, items)
+        trama = blind_window.print_blind_window(screen, blinds_functions, items)
     elif current_screen == 7:
-        settings_window.print_settings_window(screen, settings_functions, items)
-    
+        settings_window.print_settings_window(screen, settings_functions, items, re)
+
+    writeTrama()
+    # print(trama)
     pygame.display.flip()
 
-# Closing the socket
 # tcpip.close()
 
 # Closing the app
